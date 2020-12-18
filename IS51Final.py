a9a38a3fecaca82e451a35c986a905fc191769f6
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-12-17 18:43:05 -0800

    finishes psuedo-code

--- IS51Final.py
+++ IS51Final.py
@@ -16,3 +16,49 @@ after it check all the grades it will then find the percentage above class avera
 then it will display the percentage above class average
 then it will close the file
 """
+
+"""
+main():
+    class_avg(txt_file)
+    calculate_percentage_above_average(txt_file, average)
+
+class_avg(txt_file):
+    total = 0
+    number of grades = 0
+    average = 0
+
+    read .txt file
+
+    go by line in .txt file
+    total = sum of each grade
+    number_of_grades = each time you read a line in txt file
+    average = toatl/number_of_grades
+
+    print number_of_grades
+    print average
+
+    close file
+
+    return average
+
+calculate_percent_above_average(txt_file, average):
+    average_grade = average
+    students_above_average = 0
+    total_students = 0
+
+    read .txt file
+
+    go line by line in .txt file
+        count student
+        if student_grade > average_grade
+            students_above_average += 1
+        percentage_above_average = (students_above_average/total_students)*100
+
+    print percentage_above_average
+
+    close file
+
+    return percentage_above_average
+
+main()
+"""
