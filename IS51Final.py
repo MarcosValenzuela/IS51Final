57340ea4443897ccbba5c48a348a86cf9928f74a
MarcosValenzuela <marcosvalezuela@mail.fresnostate.edu>
2020-12-17 18:29:18 -0800

    finish structured-english

---
+++ IS51Final.py
@@ -0,0 +1,18 @@
+"""
+This program will read be able to find the number of grades, the average grade, and the percentage above
+average grade from a text file
+
+It will calll a function called class_avg in which it will
+open a file then it will sum the amount of grades in the file
+sum the grades then will find the class average
+it will print out the amount of grades and the class average
+then it will close the file
+
+It then will find the percentage average that is above the class average
+to do this it will open a text file
+it will then check each grade to comapre to the class average
+if the grade is above the class average it will count it 
+after it check all the grades it will then find the percentage above class average
+then it will display the percentage above class average
+then it will close the file
+"""
